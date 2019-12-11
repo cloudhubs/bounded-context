56599681123a9c1e90153aec428c86fbb3dc4b5a
Ian Laird <ian.c.laird@hotmail.com>
2019-12-11 16:22:03 -0600

    added the file

---
+++ src/test/java/edu/baylor/ecs/prophet/bounded/context/FileManager.java
@@ -0,0 +1,15 @@
+package edu.baylor.ecs.prophet.bounded.context;
+
+import com.google.gson.Gson;
+import edu.baylor.ecs.cloudhubs.prophetdto.systemcontext.SystemContext;
+
+import java.io.*;
+
+public class FileManager {
+
+    public SystemContext readSystemContextFromFile(String fileName) throws FileNotFoundException {
+        Gson gson = new Gson();
+        Reader in = new FileReader(fileName);
+        return gson.fromJson(in, SystemContext.class);
+    }
+}
